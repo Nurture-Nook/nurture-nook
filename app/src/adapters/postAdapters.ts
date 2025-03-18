63cfc11f3bbd17cbb6b377a9d5ba9ae14860c0e8
Catalina Dinozo <csdinozo@gmail.com>
2025-03-18 14:24:52 -0400

    post adapter by search function + likely queries

---
+++ app/src/adapters/postAdapters.ts
@@ -0,0 +1,20 @@
+import { handleFetch } from './handleFetch.ts';
+
+const baseUrl = '';
+
+export const getPostsBySearch = async (searchTerm: string, category = null, exclude = []) => {
+    let url = baseUrl + `/search?query=${encodeURIComponent(searchTerm)}`;
+
+    if (category !== null) url += `&category=${encodeURIComponent(category)}`;
+
+    if (exclude.length > 0) url += `&exclude=${exclude.map(encodeURIComponent).join(',')}`;
+
+    const [data, error] = await handleFetch(url);
+
+    if (error) {
+        console.error(error);
+        return [null, error];
+    }
+
+    return [data.posts, null];
+}
