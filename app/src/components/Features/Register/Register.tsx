bbbefc3e462ef09e5aa34aac9f2c51df7215910d
Catalina Dinozo <catalinadinozo@Cristinas-MacBook-Air.local>
2025-08-13 16:28:17 -0400

    added formatting issue handling to Register component + need to address 404 error when fetching

--- app/src/components/Features/Register/Register.tsx
+++ app/src/components/Features/Register/Register.tsx
@@ -9,18 +9,31 @@ interface RegisterProps {
 }
 
 export const Register: React.FC<RegisterProps> = ({ onRegisterSuccess }) => {
+    const usernamePattern = /^(?=(.*[a-zA-Z]){3,})[a-zA-Z0-9_]{3,15}$/;
+    const passwordPattern = /^(?=(.*[a-zA-Z]){5,})(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}$/;
     const [errorText, setErrorText] = useState("");
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [usernameFormattingIssue, setUsernameFormattingIssue] = useState(false);
+    const [passwordFormattingIssue, setPasswordFormattingIssue] = useState(false);
+    const [emailFormattingIssue, setEmailFormattingIssue] = useState(false);
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         setErrorText('');
+        setUsernameFormattingIssue(false);
+        setPasswordFormattingIssue(false);
+        setEmailFormattingIssue(false);
 
         if (!email || !username || !password || !passwordConfirm) return setErrorText("All Fields are Required");
+
+        if (!usernamePattern.test(username)) setUsernameFormattingIssue(true);
+        if (!passwordPattern.test(password)) setPasswordFormattingIssue(true);
+        if (!emailPattern.test(email)) setEmailFormattingIssue(true);
 
         if (password != passwordConfirm) return setErrorText("Password Confirmation Must Match Password");
 
@@ -95,6 +108,24 @@ export const Register: React.FC<RegisterProps> = ({ onRegisterSuccess }) => {
 
                 <button>Register</button>
             </form>
+
+            { usernameFormattingIssue ? <>
+                <p>Username Must be Between Three and 15 Characters Long. Using Only Letters (uppercase or lowercase), Numbers, or Underscores With at Least Three Letters.</p>
+                <br></br>
+                </> : <>
+            </>}
+            
+            { passwordFormattingIssue ? <>
+                <p>Password Must be Between Eight and 15 Characters Long. Containing at Least Five Letters, at Least One Number, and at Least One Special Character From "!@#$%^&*". Use only letters, numbers, and those special characters.</p>
+                <br></br>
+                </> : <>
+            </> }
+
+            { emailFormattingIssue ? <>
+                <p>Enter a Valid Email</p>
+                <br></br>
+                </> : <>
+            </> }
 
             <p>{errorText}</p>
 
